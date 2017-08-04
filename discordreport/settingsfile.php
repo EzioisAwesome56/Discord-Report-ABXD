ef6a3bd1e6674c39d14b23664ac4fc33394c32d2
EzioisAwesome56 <jedijon567@gmail.com>
2017-08-04 12:59:40 -0400

    readd some old settings

--- discordreport/settingsfile.php
+++ discordreport/settingsfile.php
@@ -15,5 +15,15 @@
 			"default" => "https://maxcdn.icons8.com/Share/icon/Logos//discord_logo1600.png",
 			"name" => "Webhook profile picture",
 		),
+		"reportPassMatches" => array(
+			"type" => "boolean",
+			"default" => "0",
+			"name" => "Report number of password matches",
+		),
+		"reportIPMatches" => array(
+			"type" => "boolean",
+			"default" => "0",
+			"name" => "Report number of IP matches",
+		),
 	);
 ?>
